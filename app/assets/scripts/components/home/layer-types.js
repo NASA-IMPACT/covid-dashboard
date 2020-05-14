09e0f884b08e5a956f4e7c5ff0414210ca99fa6b
Daniel da Silva <daniel@developmentseed.org>
2020-05-14 19:05:32 +0100

    Adds gibs population layer

--- app/assets/scripts/components/home/layer-types.js
+++ app/assets/scripts/components/home/layer-types.js
@@ -48,5 +48,27 @@ export const layerTypes = {
         }, 'admin-1-boundary-bg');
       }
     }
+  },
+  raster: {
+    hide: (mbMap, layerInfo, props) => {
+      const { id } = layerInfo;
+      if (mbMap.getSource(id)) {
+        mbMap.setLayoutProperty(id, 'visibility', 'none');
+      }
+    },
+    show: (mbMap, layerInfo, props) => {
+      const { id, source } = layerInfo;
+
+      if (mbMap.getSource(id)) {
+        mbMap.setLayoutProperty(id, 'visibility', 'visible');
+      } else {
+        mbMap.addSource(id, source);
+        mbMap.addLayer({
+          id: id,
+          type: 'raster',
+          source: id
+        }, 'admin-1-boundary-bg');
+      }
+    }
   }
 };
