76cd50c35c1891848212c8f80ebf61b540941d35
Ricardo Mestre <ricardo@duplos.org>
2020-05-19 23:24:21 +0100

    Restyle page header to include branding

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -2,6 +2,7 @@ export default {
   environment: 'production',
   appTitle: 'COVID-19 Dashboard',
   appDescription: 'COVID-19 Dashboard',
+  appVersion: 'v0.01',
   gaTrackingCode: null,
   mbToken: 'pk.eyJ1IjoiY292aWQtc3VwcG9ydCIsImEiOiJjazlhMTNweDIwMHd2M21venc1Nzdzdzh5In0.QZbkhaPpO9jRclw-dQWnDA',
   api: 'https://covid-support-api.s3.amazonaws.com/us',
