01c0c66aae2a8b5e406c383134d85a22fa151c04
Daniel da Silva <daniel@developmentseed.org>
2020-05-21 21:42:45 +0100

    Update cog endpoint

--- app/assets/scripts/components/common/layers/layer-no2.js
+++ app/assets/scripts/components/common/layers/layer-no2.js
@@ -10,7 +10,7 @@ export default {
   source: {
     type: 'raster',
     tiles: [
-      'https://h4ymwpefng.execute-api.us-east-1.amazonaws.com/v1/{z}/{x}/{y}@1x?url=s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{date}_Col3_V4.nc.tif&resampling_method=bilinear&bidx=1&rescale=0%2C1e16&color_map=magma&color_formula=gamma r {gamma}'
+      'https://8ib71h0627.execute-api.us-east-1.amazonaws.com/v1/{z}/{x}/{y}@1x?url=s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{date}_Col3_V4.nc.tif&resampling_method=bilinear&bidx=1&rescale=0%2C1e16&color_map=magma&color_formula=gamma r {gamma}'
     ]
   },
   swatch: {
