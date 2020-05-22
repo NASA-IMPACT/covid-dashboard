89f73dc3f8f38d6a36b778f3e3d7441355d3ca6e
Daniel da Silva <daniel@developmentseed.org>
2020-05-22 18:05:34 +0100

    Fix 404 page

--- app/assets/scripts/components/uhoh/index.js
+++ app/assets/scripts/components/uhoh/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import App from '../common/app';
@@ -8,10 +9,16 @@ import {
   InpageHeaderInner,
   InpageHeadline,
   InpageTitle,
-  InpageBody,
-  InpageBodyInner
+  InpageBody
 } from '../../styles/inpage';
 import Prose from '../../styles/type/prose';
+import Constrainer from '../../styles/constrainer';
+import { Fold } from '../../styles/fold';
+
+const PageProse = styled(Prose)`
+  grid-row: 1;
+  grid-column: span 8;
+`;
 
 export default class UhOh extends React.Component {
   render () {
@@ -26,13 +33,15 @@ export default class UhOh extends React.Component {
             </InpageHeaderInner>
           </InpageHeader>
           <InpageBody>
-            <InpageBodyInner>
-              <Prose>
-                <p>We were not able to find the page you are looking for. It may have been archived or removed.</p>
-                <p>You might find an older snapshot of this page at the <a href='http://archive.org/web/' title='Find on Internet Archive'>Internet Archive</a>.<br /> If you think this page should be here let us know via <a href='mailto:' title='Send us an email'>email</a>.</p>
-                <p><Link to='/' title='Visit the homepage'>Visit the homepage</Link></p>
-              </Prose>
-            </InpageBodyInner>
+            <Fold>
+              <Constrainer>
+                <PageProse>
+                  <p>We were not able to find the page you are looking for. It may have been archived or removed.</p>
+                  <p>You might find an older snapshot of this page at the <a href='http://archive.org/web/' title='Find on Internet Archive'>Internet Archive</a>.<br /> If you think this page should be here let us know via <a href='mailto:' title='Send us an email'>email</a>.</p>
+                  <p><Link to='/' title='Visit the homepage'>Visit the homepage</Link></p>
+                </PageProse>
+              </Constrainer>
+            </Fold>
           </InpageBody>
         </Inpage>
       </App>
