cc29728dbe34785433c7dba781564eb81da2c368
Olaf Veerman <olaf.veerman@gmail.com>
2020-06-01 12:03:57 -0400

    Add About copy

--- app/assets/scripts/components/about/index.js
+++ app/assets/scripts/components/about/index.js
@@ -12,8 +12,7 @@ import {
 } from '../../styles/inpage';
 
 import {
-  Fold,
-  FoldTitle
+  Fold
 } from '../../styles/fold';
 
 import Constrainer from '../../styles/constrainer';
@@ -41,11 +40,32 @@ export default class About extends React.Component {
             <Fold>
               <Constrainer>
                 <AboutProse>
-                  <FoldTitle>The tool</FoldTitle>
                   <p>
-                    The COVID EO dashbord provides streaming data about the
-                    pandemic to inform decisionmakers in government, community
-                    leaders, health responders, and the business community.
+                    As the world moved indoors to shelter from the global pandemic sparked by the novel coronavirus, we
+                    could perceive changes on our planet. The sky seemed a little bluer, the air a little fresher, the animals in
+                    our yards more abundant.
+                  </p>
+                  <p>
+                    NASA’s continuous and sometimes near-real-time measurements of Earth allow for understanding both the systems
+                    changes themselves and the potential impact on economies and society during the pandemic – and as the world
+                    slowly returns to operations.
+                  </p>
+                  <p>
+                    This dashboard features data collected and analyzed by the National Aeronautics and Space Administration (NASA).
+                    Information about Earth systems is gathered by a fleet of powerful global Earth-Observing satellites, instruments
+                    aboard the International Space Station, from airborne science campaigns, and via ground observations. With this
+                    data we have been able to monitor some of those changes andand this is allowing us to track and compare these
+                    changes over time.
+                  </p>
+                  <p>
+                    NASA will further expand the impact of the data sets presented in this dashboard in collaboration with
+                    European Space Agency (ESA) and the Japan Aerospace Exploration Agency (JAXA), who also have created their own
+                    online data portals to provide an even richer picture of what is happening on our home planet during this time of
+                    world crisis by developing a fourth dashboard that will combine the unique data of each agency for more comparisons
+                    and deeper understandings now and over time.
+                  </p>
+                  <p>
+                    To learn more about NASA Earth Science, go to <a href='https://nasa.gov/earth'>nasa.gov/earth</a>.
                   </p>
                 </AboutProse>
               </Constrainer>
