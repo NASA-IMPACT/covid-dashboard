9ddc2184f7f4a3ea8094f927ef3eb843986015da
Olaf Veerman <olaf.veerman@gmail.com>
2020-06-04 21:22:22 -0400

    Update Nightlights copy

--- app/assets/scripts/components/indicators/indicator-nightlights.js
+++ app/assets/scripts/components/indicators/indicator-nightlights.js
@@ -3,16 +3,40 @@ import styled from 'styled-components';
 
 import Prose from '../../styles/type/prose';
 import Constrainer from '../../styles/constrainer';
+import Gridder from '../../styles/gridder';
+import InpageHGroup from '../../styles/inpage-hgroup';
 import { Fold } from '../../styles/fold';
+
+import { glsp } from '../../styles/utils/theme-values';
 
 const BMProse = styled(Prose)`
   grid-row: 1;
   grid-column: span 8;
 `;
 
+const FactsFold = styled(Fold)`
+  padding-bottom: ${glsp(6)};
+
+  ${Gridder} {
+    align-items: center;
+  }
+
+  /* stylelint-disable-next-line */
+  ${InpageHGroup} {
+    grid-row: 1;
+    grid-column: content-start / content-7;
+  }
+
+  ${Prose} {
+    grid-column: content-start / content-end;
+    grid-row: 2;
+    margin-bottom: ${glsp(2)};
+  }
+`;
+
 const metadata = {
   id: 'bm',
-  name: 'Nightlights',
+  name: 'Nighttime Lights',
   color: '#2276AC'
 };
 
@@ -24,31 +48,115 @@ class BMLongForm extends React.Component {
           <Constrainer>
             <BMProse>
               <p>
-                In addition to their beauty, the images produced from NASA’s Nightlights products gives us an extraordinary view of
-                human activity over time. NASA’s Nightlights products contribute valuable information to help track changes in human
-                activity during the pandemic, and they are vital to ongoing research on urbanization and human development, light
-                pollution, illegal fishing, fires, and disaster impact and recovery.
+                Images of the Earth at night give us an extraordinary view of human activity over time. The nighttime environment
+                illuminates Earth features like city infrastructure, lightning flashes, fishing boats navigating open water, gas
+                flares, aurora, and natural hazards like lava flowing from an active volcano. Paired with the moonlight, researchers
+                can also spot snow and ice, as well as other reflective surfaces that allow nighttime land and ocean analysis.
               </p>
               <p>
-                Satellite-derived nighttime lights are derived from data collected by the Visible Infrared Radiometer Suite (VIIRS)
-                Day/Night Band (DNB) on the Suomi-National Polar-Orbiting Partnership (Suomi-NPP) platform, a joint NOAA (National
-                Oceanic and Atmospheric Administration) and NASA satellite. The data is calibrated across time, validated against
-                ground measurements, and includes science-quality indicators to effectively track changes by removing clouds, stray
-                light from snow, the Moon, and other extraneous sources.
-              </p>
-              <p>
-                The products featured are the 500-meter (VNP46) and the 30-meter Nightlights High Definition (HD) nighttime lights.
-                Here, the Nightlights HD downscales radiances from the 500-meter product to the street level using optical imagery
-                from Landsat 8, a NASA and USGS (United States Geological Survey) satellite, along with OpenStreetMap ancillary layers.
-                This helps visualize neighborhoods and commercial centers that have less activity – or closures – due to social
-                distancing restrictions.
-              </p>
-              <p>
-                For more information, please visit the Nightlights website at <a href='https://blackmarble.gsfc.nasa.gov/'>blackmarble.gsfc.nasa.gov</a>.
+                During the COVID-19 pandemic, researchers are using night light observations to track variations in energy use,
+                migration, and transportation in response to social distancing and lockdown measures.
               </p>
             </BMProse>
           </Constrainer>
         </Fold>
+
+        <FactsFold>
+          <Gridder>
+            <InpageHGroup
+              title='Scientific Research'
+              dashColor={metadata.color}
+            />
+            <Prose>
+              <p>
+                Nightlights data are collected by the
+                <a href='https://ladsweb.modaps.eosdis.nasa.gov/missions-and-measurements/viirs/'>Visible Infrared Radiometer Suite (VIIRS) Day/Night Band (DNB)</a>
+                on the Suomi-National Polar-Orbiting Partnership (Suomi-NPP) platform,
+                a joint NOAA (National Oceanic and Atmospheric Administration) and NASA
+                satellite. The images are produced by <a href='https://blackmarble.gsfc.nasa.gov/'>NASA’s Black Marble</a> products suite.
+                All data are calibrated daily, corrected, and validated with ground
+                measurements for science-ready analysis.
+              </p>
+              <p>
+                <a href='https://science.nasa.gov/earth-science/rrnes-awards'>New research</a>
+                funded by NASA’s Rapid Response and Novel Research in the Earth Sciences
+                (RRNES) program seeks to better understand what nightlights can tell us
+                about the impacts of COVID-19.
+              </p>
+            </Prose>
+          </Gridder>
+        </FactsFold>
+
+        <FactsFold>
+          <Gridder>
+            <InpageHGroup
+              title='Interpreting the Data'
+              dashColor={metadata.color}
+            />
+            <Prose>
+              <p>
+                Each spotlight city has a slider for turning night lights on and off. The
+                darker purple indicates fewer night lights while the lighter yellow
+                indicates more night lights. By comparing regions before and after
+                guidelines to shelter-in-place began, researchers are able to visualize
+                the extent to which social distancing measures impacted various economic
+                activities based on whether or not light pollution increased or decreased,
+                which highways were shut down, and which cities stayed the same.
+              </p>
+              <p>
+                The products featured are 500-meter (VNP46) and 30-meter Black Marble
+                High Definition (HD) nighttime lights. Black Marble HD downscales radiances
+                from the 500-meter product to street level using optical imagery from
+                Landsat 8, a NASA and USGS (U.S. Geological Survey) satellite, along with
+                OpenStreetMap ancillary layers. This helps visualize neighborhoods and
+                commercial centers that have less activity – or closures – due to social
+                distancing restrictions.
+              </p>
+            </Prose>
+          </Gridder>
+        </FactsFold>
+
+        <FactsFold>
+          <Gridder>
+            <InpageHGroup
+              title='Credits'
+              dashColor={metadata.color}
+            />
+            <Prose>
+              <p>
+                Black Marble data courtesy of Universities Space Research Association (USRA)
+                Earth from Space Institute (EfSI) and NASA Goddard Space Flight Center&apos;s
+                Terrestrial Information Systems Laboratory using VIIRS day-night band data
+                from the Suomi National Polar-orbiting Partnership and Landsat-8 Operational
+                Land Imager (OLI) data from the U.S. Geological Survey.
+              </p>
+            </Prose>
+          </Gridder>
+        </FactsFold>
+
+        <FactsFold>
+          <Gridder>
+            <InpageHGroup
+              title='Additional resources'
+              dashColor={metadata.color}
+            />
+            <Prose>
+              <p>NASA Features</p>
+              <ul>
+                <li><a href='https://earthobservatory.nasa.gov/images/146481/nighttime-images-capture-change-in-china' target='_blank' rel='noopener noreferrer'>Nighttime Images Capture Change In China </a></li>
+              </ul>
+              <p>Explore the Data</p>
+              <ul>
+                <li><a href='https://earthdata.nasa.gov/learn/articles/feature-articles/nighttime-images-wuhan' target='_blank' rel='noopener noreferrer'>Nighttime Images Show Changes In Human Activity</a></li>
+              </ul>
+              <p>Explore the Missions</p>
+              <ul>
+                <li><a href='https://blackmarble.gsfc.nasa.gov/' target='_blank' rel='noopener noreferrer'>NASA’s Black Marble</a></li>
+                <li><a href='https://www.nasa.gov/mission_pages/NPP/main/index.html' target='_blank' rel='noopener noreferrer'>Suomi National Polar-orbiting Partnership (Suomi NPP)</a></li>
+              </ul>
+            </Prose>
+          </Gridder>
+        </FactsFold>
       </React.Fragment>
     );
   }
