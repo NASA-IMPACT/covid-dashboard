029aaf12c4dae4c8cfc726f375c245ab5c46a6be
Ricardo Mestre <ricardo@duplos.org>
2020-06-12 23:58:19 +0100

    Restrcture and restyle homepage
    Work in progress.

--- app/assets/scripts/components/home/index.js
+++ app/assets/scripts/components/home/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+import { themeVal } from '../../styles/utils/general';
+import collecticon from '../../styles/collecticons';
+import { visuallyHidden } from '../../styles/helpers';
+
 import App from '../common/app';
 import Button from '../../styles/button/button';
 import {
@@ -13,13 +17,74 @@ import {
   InpageBody
 } from '../../styles/inpage';
 import InpageHGroup from '../../styles/inpage-hgroup';
-import {
-  PageConstrainer
-} from '../../styles/hub-pages';
 import Prose from '../../styles/type/prose';
+import { headingAlt } from '../../styles/type/heading';
 
 import { filterComponentProps } from '../../utils/utils';
 import { glsp } from '../../styles/utils/theme-values';
+import media from '../../styles/utils/media-queries';
+
+const Intro = styled.section`
+  position: relative;
+  height: 100%;
+  background: ${themeVal('color.baseAlphaA')};
+`;
+
+const IntroCopy = styled.div`
+  position: absolute;
+  top: ${glsp()};
+  left: ${glsp()};
+  z-index: 10;
+  background: ${themeVal('color.surface')};
+  padding: ${glsp()};
+  border-radius: ${themeVal('shape.rounded')};
+  max-height: calc(100% - ${glsp(2)});
+  width: 100%;
+  max-width: 16rem;
+  overflow-y: auto;
+
+  ${media.smallUp`
+    max-width: 24rem;
+  `}
+
+  ${media.mediumUp`
+    top: ${glsp(2)};
+    left: ${glsp(2)};
+    padding: ${glsp(2)};
+    max-height: calc(100% - ${glsp(4)});
+    max-width: 28rem;
+  `}
+
+  ${media.largeUp`
+    max-height: calc(100% - ${glsp(6)});
+    max-width: 44rem;
+  `}
+`;
+
+const IntroTitle = styled.h1`
+  margin: 0 0 ${glsp()} 0;
+
+  small {
+    ${headingAlt()}
+    display: block;
+
+    ${media.mediumUp`
+      font-size: 1rem;
+      line-height: 1;
+    `}
+  }
+
+  strong {
+    display: block;
+    font-size: 2rem;
+    line-height: 2.5rem;
+    letter-spacing: -0.016em;
+  }
+`;
+
+const IntroProse = styled(Prose)`
+  background: transparent;
+`;
 
 const IntroActions = styled.div`
   display: flex;
@@ -37,6 +102,24 @@ const IntroActions = styled.div`
   }
 `;
 
+const IntroMedia = styled.figure`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > * {
+    ${visuallyHidden()}
+  }
+
+  &::before {
+    ${collecticon('map')}
+    opacity: 0.08;
+    font-size: 48rem;
+  }
+`;
+
 const propsToFilter = ['size', 'useIcon', 'variation'];
 const CleanNavLink = filterComponentProps(NavLink, propsToFilter);
 
@@ -44,70 +127,79 @@ export default class Home extends React.Component {
   render () {
     return (
       <App pageTitle='Home'>
-        <Inpage>
+        <Inpage isMapCentric>
           <InpageHeader>
             <InpageHeaderInner>
               <InpageHeadline>
-                <InpageTitle>Welcome to the COVID-19 Data&nbsp;Dashboard</InpageTitle>
+                <InpageTitle>Welcome</InpageTitle>
               </InpageHeadline>
             </InpageHeaderInner>
           </InpageHeader>
           <InpageBody>
-            <PageConstrainer>
-              <Prose>
-                <p>
-                  As communities around the world have changed their behavior in response
-                  to the spread of COVID-19, NASA satellites have observed associated
-                  changes in the environment.
-                </p>
-                <p>
-                  The NASA COVID-19 data dashboard can be used to explore these changes
-                  on a global scale. Powered by publicly available NASA Earth observing
-                  data, the dashboard highlights 10 key indicators – 4 environmental and
-                  6 economic – that show how the planet is responding to our changing
-                  behavior in response to COVID-19.
-                </p>
-                <p>
-                  Use the dashboard to interact with real NASA data and investigate how
-                  social distancing measures and regional shelter-in-place guidelines have
-                  affected Earth’s air, land, and water. Explore individual &apos;Spotlight Areas&apos;
-                  to see how the indicators in each specific location have changed through
-                  time.
-                </p>
-              </Prose>
-              <InpageHGroup
-                title='Explore the site'
-              />
-              <IntroActions>
-                <Button
-                  as={CleanNavLink}
-                  size='large'
-                  title='View the global map'
-                  to='/getstarted'
-                  variation='primary-raised-dark'
-                >
-                  Getting Started
-                </Button>
-                <Button
-                  as={CleanNavLink}
-                  size='large'
-                  title='View the Indicators'
-                  to='/indicators'
-                  variation='primary-raised-dark'
-                >
-                  Indicators
-                </Button>
-                <Button
-                  as={CleanNavLink}
-                  size='large'
-                  title='View the Spotlight locations'
-                  to='/spotlight'
-                  variation='primary-raised-dark'
-                >
-                  Spotlight locations
-                </Button>
-              </IntroActions>
-            </PageConstrainer>
+            <Intro>
+              <IntroCopy>
+                <IntroTitle>
+                  <small>Welcome to the</small>
+                  <strong>COVID-19 Data Dashboard</strong>
+                </IntroTitle>
+                <IntroProse>
+                  <p>
+                    As communities around the world have changed their behavior
+                    in response to the spread of COVID-19, NASA satellites have
+                    observed associated changes in the environment.
+                  </p>
+                  <p>
+                    The NASA COVID-19 data dashboard can be used to explore
+                    these changes on a global scale. Powered by publicly
+                    available NASA Earth observing data, the dashboard
+                    highlights 10 key indicators – 4 environmental and 6
+                    economic – that show how the planet is responding to our
+                    changing behavior in response to COVID-19.
+                  </p>
+                  <p>
+                    Use the dashboard to interact with real NASA data and
+                    investigate how social distancing measures and regional
+                    shelter-in-place guidelines have affected Earth’s air, land,
+                    and water. Explore individual &apos;Spotlight Areas&apos; to
+                    see how the indicators in each specific location have
+                    changed through time.
+                  </p>
+                </IntroProse>
+                <IntroActions>
+                  <Button
+                    as={CleanNavLink}
+                    size='large'
+                    title='View the global map'
+                    to='/getstarted'
+                    variation='primary-raised-dark'
+                  >
+                    Getting Started
+                  </Button>
+                  <Button
+                    as={CleanNavLink}
+                    size='large'
+                    title='View the Indicators'
+                    to='/indicators'
+                    variation='primary-raised-dark'
+                  >
+                    Indicators
+                  </Button>
+                  <Button
+                    as={CleanNavLink}
+                    size='large'
+                    title='View the Spotlight locations'
+                    to='/spotlight'
+                    variation='primary-raised-dark'
+                  >
+                    Spotlight locations
+                  </Button>
+                </IntroActions>
+              </IntroCopy>
+
+              <IntroMedia>
+                <p>A map will appear here.</p>
+              </IntroMedia>
+            </Intro>
           </InpageBody>
         </Inpage>
       </App>
