a31c58a34eb01d390a765de0640da6254bc0ef86
Olaf Veerman <olaf.veerman@gmail.com>
2020-06-18 08:38:13 -0400

    Add stories to the homepage

--- app/assets/scripts/components/home/stories/story-be.js
+++ app/assets/scripts/components/home/stories/story-be.js
@@ -2,6 +2,6 @@ export default {
   spotlightId: 'be',
   title: 'The drop in CO2 emission',
   prose: 'Beijing has some indication of reduction in activity based on the drop in CO2 during lockdown.',
-  layers: ['nightlights-hd'],
+  layers: ['co2-diff'],
   link: '/explore/be'
 };
---
+++ app/assets/scripts/components/home/stories/story-la.js
@@ -0,0 +1,7 @@
+export default {
+  spotlightId: 'la',
+  title: '19% reduction in shipping',
+  prose: 'Los Angeles has the busiest port in the United States, which this year saw a 19% reduction in shipping cargo volumes compared to 2019. NASA researchers are using artificial intelligence techniques and high resolution satellite imagery to track shipping activity over time.',
+  layers: ['no2'],
+  link: '/explore/la'
+};
