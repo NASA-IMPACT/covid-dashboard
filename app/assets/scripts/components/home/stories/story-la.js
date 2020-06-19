b72fcebce34b5ce8a503d441a4d5d14464609071
Olaf Veerman <olaf.veerman@gmail.com>
2020-06-19 17:53:52 -0400

    Enable ship detection layer on homepage story LA

--- app/assets/scripts/components/home/stories/story-la.js
+++ app/assets/scripts/components/home/stories/story-la.js
@@ -2,6 +2,6 @@ export default {
   spotlightId: 'la',
   title: '19% reduction in shipping',
   prose: 'Los Angeles has the busiest port in the United States, which this year saw a 19% reduction in shipping cargo volumes compared to 2019. NASA researchers are using artificial intelligence techniques and high resolution satellite imagery to track shipping activity over time.',
-  layers: ['no2'],
+  layers: ['detection-ship'],
   link: '/explore/la'
 };
