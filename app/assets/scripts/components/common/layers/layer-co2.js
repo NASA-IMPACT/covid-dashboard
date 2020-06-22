6ad4bafb195d5f0c5153516ab9ee0569a30e6f47
Jeevan Farias <jeevan@developmentseed.org>
2020-06-22 10:17:57 -0400

    Upate co2 color scale

--- app/assets/scripts/components/common/layers/layer-co2.js
+++ app/assets/scripts/components/common/layers/layer-co2.js
@@ -17,7 +17,7 @@ export default {
   source: {
     type: 'raster',
     tiles: [
-      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/xco2/xco2_15day_mean.{date}.tif&resampling_method=bilinear&bidx=1&rescale=0.0004%2C0.00042&color_map=reds&color_formula=gamma r {gamma}`
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/xco2/xco2_15day_mean.{date}.tif&resampling_method=bilinear&bidx=1&rescale=0.000408%2C0.000419&color_map=rdylbu_r&color_formula=gamma r {gamma}`
 
     ]
   },
@@ -31,7 +31,7 @@ export default {
     source: {
       type: 'raster',
       tiles: [
-      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/xco2/xco2_15day_base.{date}.tif&resampling_method=bilinear&bidx=1&rescale=0.0004%2C0.00042&color_map=reds&color_formula=gamma r {gamma}`
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/xco2/xco2_15day_base.{date}.tif&resampling_method=bilinear&bidx=1&rescale=0.000408%2C0.000419&color_map=rdylbu_r&color_formula=gamma r {gamma}`
       ]
     }
   },
@@ -39,11 +39,12 @@ export default {
   swatch: indicatorGroupColors['greenhouse-gas'],
   legend: {
     type: 'gradient-adjustable',
-    min: 'less',
-    max: 'more',
+    min: '408',
+    max: '419',
     stops: [
-      '#FFFFFF',
-      '#FF0000'
+      '#FF0000',
+      '#FFFF00',
+      '#0000FF'
     ]
   },
   info: null
