4a0d99c49c7c318f4d9e51b6552cafcd5d164073
Jeevan Farias <jeevan@developmentseed.org>
2020-06-23 16:46:36 -0400

    Change no2 scaling factor

--- app/assets/scripts/components/common/layers/layer-no2.js
+++ app/assets/scripts/components/common/layers/layer-no2.js
@@ -15,7 +15,7 @@ export default {
   source: {
     type: 'raster',
     tiles: [
-      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{date}_Col3_V4.nc.tif&resampling_method=bilinear&bidx=1&rescale=0%2C1e16&color_map=custom_no2&color_formula=gamma r {gamma}`
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{date}_Col3_V4.nc.tif&resampling_method=bilinear&bidx=1&rescale=0%2C2e16&color_map=custom_no2&color_formula=gamma r {gamma}`
     ]
   },
   paint: {
