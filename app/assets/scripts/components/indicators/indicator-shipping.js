806bd9273014fd3017a1c4a34e84930468391607
Jeevan Farias <jeevan@developmentseed.org>
2020-06-24 17:04:54 -0400

    Change / to ,

--- app/assets/scripts/components/indicators/indicator-shipping.js
+++ app/assets/scripts/components/indicators/indicator-shipping.js
@@ -248,7 +248,7 @@ class ShippingLongForm extends React.Component {
                 Los Angeles has the busiest port in the United States, which
                 this year saw a 19% reduction in shipping cargo volumes compared
                 to 2019, according to the Port of Los Angeles. Image Credit:
-                Planet Labs/NASA.
+                Planet Labs, NASA.
               </figcaption>
             </MediaImage>
           </Gridder>
