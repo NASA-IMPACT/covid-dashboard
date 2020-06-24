7831510b361280289ce8549158adad5252df7a57
Olaf Veerman <olaf.veerman@gmail.com>
2020-06-23 22:10:54 -0400

    Remove indicator paragraphs from spotlight texts Move them to the API

--- app/assets/scripts/components/spotlight/single/summaries.js
+++ app/assets/scripts/components/spotlight/single/summaries.js
@@ -16,29 +16,6 @@ const summaries = {
         nitrogen dioxide (NO2) and carbon dioxide (CO2) emissions from fossil
         fuel combustion and other human activities.
       </p>
-      <p>
-        Using observations from the NASA Orbiting Carbon Observatory 2 (OCO-2)
-        and Japan’s Greenhouse gases Observing Satellite (GOSAT), scientists
-        were able to identify enhancements in ambient CO2 concentrations from
-        things like fossil fuel emissions over cities, which are only detectable
-        lower down in the atmosphere.
-      </p>
-      <p>
-        In Beijing, while enhancements in CO2 concentrations vary substantially
-        from month-to-month generally, the 2020 levels are much lower than in
-        earlier years. While this behavior is consistent with reported
-        coronavirus-related reductions in fossil fuel emissions, it is important
-        to remember that these results include variations in CO2 concentrations
-        not only from COVID-19 shutdowns, but also from other processes such as
-        photosynthesis and respiration from plants and transport by passing
-        weather systems.
-      </p>
-      <p>
-        Additionally, researchers are using night light observations to track
-        variations in Beijing’s energy use and other economic indicators based
-        on whether light pollution increased or decreased, which highways were
-        shut down, and which cities stayed the same.
-      </p>
     </>
   ),
   gh: (
@@ -50,14 +27,6 @@ const summaries = {
         an indicator of economic activity before and after novel coronavirus
         lockdowns began.
       </p>
-      <p>
-        Nightlight observations from space track variations in energy use,
-        migration, and transportation. By comparing regions before and after
-        guidelines to shelter-in-place began, researchers visualize the extent
-        to which social distancing measures affected various economic activities
-        based on whether light pollution increased or decreased, which highways
-        were shut down, and which cities stayed the same.
-      </p>
     </>
   ),
   du: (
@@ -67,13 +36,7 @@ const summaries = {
         France. Located on the busy North Sea, the port is well known for the
         comings and goings of heavy cargo barges. In early March, following
         French government guidance, the port began to limit activity in response
-        to the novel coronavirus. Researchers are using night lights to track
-        variations in energy use, migration, and transportation at the port in
-        response to social distancing and lockdown measures. They are able to
-        visualize the extent social distancing measures impacted various
-        economic activities based on whether light pollution increased or
-        decreased, which highways were shut down, and which cities stayed the
-        same.
+        to the novel coronavirus.
       </p>
     </>
   ),
@@ -84,18 +47,7 @@ const summaries = {
         restrictions in an attempt to slow the spread of the novel coronavirus.
         These orders went into effect for the whole state on March 19, 2020,
         including for the city of Los Angeles and its approximately 4 million
-        residents.
-      </p>
-      <p>
-        During the lockdown, LA saw cleaner air and quieter streets:
-        observations from NASA’s Ozone Monitoring Instrument (OMI) detected a
-        31% decrease in nitrogen dioxide, a common air pollutant, as car traffic
-        and other activity came to a relative halt. Nightlights data also
-        revealed darkened commercial zones where there was once bustling
-        activity. The Port of Los Angeles, the busiest port in the U.S. and one
-        of the busiest ports in the world, reported overall shipping cargo
-        volumes were down 19% for the first five months of 2020, compared to
-        2019.
+        residents. During the lockdown, LA saw cleaner air and quieter streets.
       </p>
     </>
   ),
@@ -114,41 +66,6 @@ const summaries = {
         neighborhoods, commercial centers, and more changed due to social
         distancing measures.
       </p>
-      <p>
-        Shelter-in-place orders have led to significant but temporary reductions
-        in nitrogen dioxide (NO2) and carbon dioxide (CO2) emissions from fossil
-        fuel combustion and other human activities, improving local air quality
-        at least for a short time. Beginning in March, the Northeast U.S. saw
-        reductions of 30% across most major metropolitan areas, including New
-        York City. Observations and analysis of CO2 concentration enhancements
-        (i.e., fossil fuel emissions from cities that contribute to higher
-        levels of CO2 lower down in the atmosphere) in New York City found that
-        CO2 values were higher than normal in January 2020, close to normal for
-        February, and lower in March, as lockdowns were imposed.
-      </p>
-      <p>
-        It is important to note, however, that variations in CO2 concentrations
-        are also affected by other processes such as photosynthesis and
-        respiration by plants, and transport by passing weather systems.
-      </p>
-      <p>
-        Citywide restrictions are expected to dramatically alter the amount and
-        location of nutrients flowing into New York’s Long Island Sound,
-        possibly altering water quality. Data from NASA, the U.S. Geological
-        Survey, and the European Commission’s Copernicus satellite missions show
-        changes in both Long Island Sound and New York Harbor water quality in
-        both chlorophyll-a concentrations (algae) and sediments (how murky the
-        water is). However, both chlorophyll-a concentrations and sediment
-        levels fluctuate naturally based on a variety of factors, including
-        natural geography and the weather. More work is ongoing to assess the
-        main factors driving these changes.
-      </p>
-      <p>
-        As with other cities, researchers can compare the extent to which social
-        distancing measures impacted various economic activities in New York
-        City based on whether light pollution increased or decreased, which
-        highways were shut down, and which areas stayed the same.
-      </p>
     </>
   ),
   sf: (
@@ -161,23 +78,8 @@ const summaries = {
         approximately 6.7 million residents in the area were asked to stay home.
       </p>
       <p>
-        Nighttime lights data show an abrupt decrease in night lights
-        corresponding to the onset of the shelter-in-place order. The location
-        of decreased lights – predominantly in the central business districts,
-        market areas, and entertainment districts – can be identified in the
-        “before” and “after” imagery.
-      </p>
-      <p>
         Satellite data reveal clearing of the air and improvements in water
-        quality during the lockdown. According to observations from the Ozone
-        Monitoring Instrument (OMI), San Francisco experienced an estimated 22%
-        reduction in nitrogen dioxide levels after restrictions were
-        implemented. Chlorophyll-a measurements (an indicator of water nutrient
-        levels and algae growth) over south San Francisco Bay, along with
-        on-the-ground reports, indicate improved water quality during the
-        shelter-in-place order. However, such effects may be obscured by
-        less-than-normal rainfall in the spring, coupled with high temperatures
-        and cloudiness in the region, which naturally limit algae growth.
+        quality during the lockdown.
       </p>
     </>
   ),
@@ -197,14 +99,6 @@ const summaries = {
         transportation, and nitrogen dioxide data track changes in
         transportation and energy use in response to social distancing and
         lockdown measures.{' '}
-      </p>
-      <p>
-        Each spotlight city has a slider for turning night lights and nitrogen
-        dioxide data layers on and off. By comparing regions before and after
-        guidelines to shelter-in-place began, researchers are able to visualize
-        the extent to which social distancing measures impacted various economic
-        activities based on whether light pollution or NO2 levels increased or
-        decreased.
       </p>
     </>
   )
