daa6d57c83b06cf7dc1cde2eb7dc5a73a2ea828f
Daniel da Silva <daniel@developmentseed.org>
2020-07-16 16:27:57 +0100

    Add bar chart and set as default for ships
    Fix #333

---
+++ app/assets/scripts/components/common/bar-chart/xaxis.layer.js
@@ -0,0 +1,30 @@
+import * as d3 from 'd3';
+import * as d3fc from '@d3fc/d3fc-axis';
+import { css } from 'styled-components';
+
+const styles = props => css`
+  /* XAxis specific styles */
+`;
+
+export default {
+  styles,
+  init: ctx => {
+    ctx.svg.append('g').attr('class', 'x axis');
+  },
+
+  update: ctx => {
+    const { svg, xScale } = ctx;
+    const { left } = ctx.margin;
+    const { height } = ctx.getSize();
+
+    const xAxis = d3fc.axisBottom(xScale)
+      .tickSize(0)
+      .tickPadding(8)
+      .ticks(4)
+      .tickFormat(d3.timeFormat('%b %y\''));
+
+    svg.select('.x.axis')
+      .attr('transform', `translate(${left},${height + 8})`)
+      .call(d3fc.axisLabelRotate(xAxis).labelRotate(45));
+  }
+};
