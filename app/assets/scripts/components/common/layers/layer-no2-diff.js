85f87c43a529d63f4722a5bd2a197a80653e4ce4
Jeevan Farias <jeevan@developmentseed.org>
2020-07-24 09:34:24 -0400

    Update no2 diff colormap

--- app/assets/scripts/components/common/layers/layer-no2-diff.js
+++ app/assets/scripts/components/common/layers/layer-no2-diff.js
@@ -13,7 +13,7 @@ export default {
   source: {
     type: 'raster',
     tiles: [
-      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/OMNO2d_HRMDifference/OMI_trno2_0.10x0.10_{date}_Col3_V4.nc.tif&resampling_method=bilinear&bidx=1&rescale=0%2C1.5e16&color_map=custom_no2&color_formula=gamma r {gamma}`
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/OMNO2d_HRMDifference/OMI_trno2_0.10x0.10_{date}_Col3_V4.nc.tif&resampling_method=bilinear&bidx=1&rescale=-0.000001%2C0.000001&color_map=rdbu_r&color_formula=gamma r {gamma}`
     ]
   },
   exclusiveWith: ['co2', 'co2-diff', 'gibs-population', 'car-count', 'nightlights-viirs', 'nightlights-hd', 'detection-ship', 'detection-multi', 'water-chlorophyll', 'water-spm', 'no2'],
@@ -24,12 +24,12 @@ export default {
     min: 'less',
     max: 'more',
     stops: [
-      '#99c5e0',
-      '#f9eaa9',
-      '#f7765d',
-      '#c13b72',
-      '#461070',
-      '#050308'
+      '#3A88BD',
+      '#C9E0ED',
+      '#E4EEF3',
+      '#FDDCC9',
+      '#DD7059'
+
     ]
   },
   info: 'Darker colors indicate higher nitrogen dioxide (NO₂) levels associated and more activity. Lighter colors indicate lower levels of NO₂ and less activity.'
