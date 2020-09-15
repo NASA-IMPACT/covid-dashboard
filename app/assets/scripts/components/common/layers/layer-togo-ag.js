a100e24779960343ca863b4c04c73e71bc2c3c89
Olaf Veerman <olaf.veerman@gmail.com>
2020-09-15 14:23:46 +0100

    Add Togo ag layer Contribute to #375

---
+++ app/assets/scripts/components/common/layers/layer-togo-ag.js
@@ -0,0 +1,19 @@
+
+import config from '../../../config';
+
+import { indicatorGroupColors } from '../../../styles/theme/theme.js';
+
+export default {
+  id: 'gibs-population',
+  name: 'Population',
+  type: 'raster',
+  source: {
+    type: 'raster',
+    tiles: [
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/Togo/togo_cropland_v7-1_cog_v2.tif&resampling_method=bilinear&bidx=1&rescale=0%2C1&color_map=inferno`
+    ]
+  },
+  exclusiveWith: [],
+  swatch: indicatorGroupColors.agriculture,
+  info: null
+};
