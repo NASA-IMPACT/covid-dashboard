829459a60ee686e6dda894cf43ce8577bf3c3f23
Aimee Barciauskas <aimee@developmentseed.org>
2020-09-25 14:55:41 -0700

    Remove dataset layers

--- app/assets/scripts/components/common/layers/layer-imerg.js
+++ app/assets/scripts/components/common/layers/layer-imerg.js
@@ -5,22 +5,22 @@ import config from '../../../config';
 import { indicatorGroupColors } from '../../../styles/theme/theme.js';
 
 export default {
-  id: 'co2',
-  name: 'CO\u2082 (Avg)',
+  id: 'imerg',
+  name: 'GPM IMERG Precipitation',
   type: 'raster-timeseries',
   timeUnit: 'day',
   domain: [
-    '2020-01-01',
-    '2020-05-17'
+    '2000-06-01',
+    '2000-06-20'
   ],
   source: {
     type: 'raster',
     tiles: [
-      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/xco2/xco2_15day_mean.{date}.tif&resampling_method=bilinear&bidx=1&rescale=0.000408%2C0.000419&color_map=rdylbu_r&color_formula=gamma r {gamma}`
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://cumulus-map-internal/cloud-optimized/GPM_3IMERGDF/3B-DAY.MS.MRG.3IMERG.{date}-S000000-E235959.V06.tif&rescale=0%2C100&color_map=rdylbu_r&color_formula=gamma r {gamma}`
 
     ]
   },
-  exclusiveWith: ['agriculture', 'no2', 'co2-diff', 'gibs-population', 'car-count', 'nightlights-viirs', 'nightlights-hd', 'detection-ship', 'detection-multi', 'water-chlorophyll', 'water-spm'],
+  exclusiveWith: [''],
   enabled: false,
   compare: {
     enabled: true,
@@ -30,16 +30,15 @@ export default {
     source: {
       type: 'raster',
       tiles: [
-      `${config.api}/{z}/{x}/{y}@1x?url=s3://covid-eo-data/xco2/xco2_15day_base.{date}.tif&resampling_method=bilinear&bidx=1&rescale=0.000408%2C0.000419&color_map=rdylbu_r&color_formula=gamma r {gamma}`
+      `${config.api}/{z}/{x}/{y}@1x?url=s3://cumulus-map-internal/cloud-optimized/GPM_3IMERGDF/3B-DAY.MS.MRG.3IMERG.{date}-S000000-E235959.V06.tif&rescale=0%2C100&color_map=rdylbu_r&color_formula=gamma r {gamma}`
       ]
     }
   },
-
   swatch: indicatorGroupColors['greenhouse-gas'],
   legend: {
     type: 'gradient-adjustable',
-    min: '< 408 ppm',
-    max: '> 419 ppm',
+    min: '0 mm/hr',
+    max: '100 mm/hr',
     stops: [
       '#313695',
       '#588cbf',
