91772078ead65c9541e1f74ac4c1753acc24324a
Aimee Barciauskas <aimee@developmentseed.org>
2020-09-26 16:41:42 -0700

    Try using redux async initial state

--- app/assets/scripts/utils/store.js
+++ app/assets/scripts/utils/store.js
@@ -1,8 +1,9 @@
 'use strict';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-
+import * as asyncInitialState from 'redux-async-initial-state';
+import { fetchJSON } from '../redux/reduxeed';
 import reducer from '../redux';
 import config from '../config';
 const { environment } = config;
@@ -20,9 +21,21 @@ const composeEnhancers =
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     : compose;
 
+// Load state function
+// Should return promise that resolves application state
+const loadStore = async function() {
+  const url = 'https://cumulus-map-internal.s3.amazonaws.com/cloud-optimized/collections/imerg.json';
+
+  const remoteLayers = await fetchJSON(url, { method: 'GET' }).then(response => {
+    return response.body;
+  });
+  return combineReducers({ remoteLayers });
+};
+
 const middlewares = applyMiddleware(
   thunkMiddleware,
-  logger
+  logger,
+  asyncInitialState.middleware(loadStore)
 );
 
 const store = createStore(
