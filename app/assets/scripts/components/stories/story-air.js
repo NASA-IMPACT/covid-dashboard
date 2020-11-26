bca98a7d4e870f119660aecc7d80a023e993a5c9
Olaf Veerman <olaf.veerman@gmail.com>
2020-11-26 23:44:00 +0000

    Update with final text for air story

--- app/assets/scripts/components/stories/story-air.js
+++ app/assets/scripts/components/stories/story-air.js
@@ -14,7 +14,7 @@ export default {
       contentComp: (
         <>
           <p>
-            When governments began implementing lockdowns at the start of the COVID-19 pandemic, scientists wondered how the atmosphere would respond to the sudden change in human behavior. With people largely confined to their homes to slow the spread of the virus, there were likely to be fewer cars, planes, and ships burning and emitting fossil fuels. Nearly a year into the pandemic, these scenarios have largely played out: during the strictest lockdown periods, locations around the world experienced substantial reductions in transportation-related fossil fuel emissions. However, those declines have ultimately proven to be short-lived, and the impacts on specific air pollutants have been varied. Today, air quality levels are beginning to approach pre-pandemic levels, and scientists are just beginning to dive into the new measurements collected throughout this unprecedented time.
+            When governments began implementing shutdowns at the start of the COVID-19 pandemic, scientists wondered how the atmosphere would respond to the sudden change in human behavior. With people largely confined to their homes to slow the spread of the novel coronavirus, scientists expected there were likely to be fewer cars, planes, and ships burning and emitting fossil fuels. As the pandemic has progressed, these scenarios have largely played out: during the strictest lockdown periods, locations around the world experienced substantial reductions in transportation-related fossil fuel emissions. The impacts on specific air pollutants have been varied and dependant on their lifespan in the atmosphere. Those pollutants with short lifespans, such as nitrogen dioxide (NO2), decreased dramatically and quickly along with emissions. Today, air quality levels are beginning to approach pre-pandemic levels, and scientists are just beginning to dive into the new measurements collected throughout this unprecedented time.
           </p>
         </>
       ),
@@ -32,11 +32,13 @@ export default {
       contentComp: (
         <>
           <p>
-            Cities are easy to spot from space. Choose any large urban area around the world, and you’re likely to see similar things: dense population centers, complex webs of highways, and, more often than not, smog. Smog is the hazy curtain of air that often hangs over cities. It occurs when nitrogen dioxide (NO<sub>2</sub>) produced from fossil fuel emissions from gasoline in cars or coal in powerplants chemically reacts with sunlight. Thick smog is harmful to breathe and can significantly reduce visibility. During lockdowns, cities around the world experienced sharp reductions in nitrogen dioxide emissions, and smog began to vanish. Skies were bluer, air was cleaner, and, in some places, views previously obscured by air pollution were suddenly revealed. In Los Angeles, nitrogen dioxide levels fell by more than 30% during the height of COVID-related lockdowns. Other large cities around the world experienced similar reductions.
+            Cities are easy to spot from space. Choose any large, urban area around the world, and you’re likely see similar things: dense population centers, complex webs of highways and, more often than not, smog. Smog is the hazy curtain of air that often hangs over cities. It occurs when nitrogen dioxide  produced from fossil fuel emissions from gasoline in cars or coal in powerplants chemically reacts with sunlight and other pollutants like carbon monoxide (CO). Thick smog is harmful to breathe and can significantly reduce visibility. During lockdowns, satellites observed sharp reductions in nitrogen dioxide emissions in cities around the world, and smog began to vanish. Skies were bluer, air was cleaner, and, in some places, views previously obscured by air pollution were suddenly revealed. In Los Angeles, NASA scientists detected that nitrogen dioxide levels fell by more than 30% during the height of COVID-related shutdowns. Other large cities around the world experienced similar reductions.
+          </p>
+          <p>
+            Imagery: Landsat 8, February 26, 2020.
           </p>
         </>
       )
-      // Data visual: Los Angeles, USA, February 2020; Landsat Image
     },
     {
       id: 'cities-experiencing-clearer-air',
@@ -48,7 +50,7 @@ export default {
           contentComp: (
             <>
               <p>
-                When Chinese authorities suspended travel and closed businesses in late January 2020 in response to the novel coronavirus, Beijing’s nearly 21 million residents saw huge drops in their local nitrogen dioxide levels. In February 2020, concentrations fell by nearly 30% compared to the previous five-year average.
+                When Chinese authorities suspended travel and closed businesses in late January 2020 in response to the novel coronavirus, Beijing’s local nitrogen dioxide levels fell dramatically. In February 2020, concentrations fell by nearly 30% compared to the previous five-year average.
               </p>
             </>
           ),
@@ -76,7 +78,7 @@ export default {
           contentComp: (
             <>
               <p>
-                Cities across South America experienced similar declines in NO<sub>2</sub>. Lima, Peru had some of the most substantial reductions, with nitrogen dioxide levels falling approximately 70% below normal levels.
+                Cities across South America experienced similar declines in nitrogen dioxide. Lima, Peru had some of the most substantial reductions, with nitrogen dioxide levels falling approximately 70% below normal levels.
               </p>
             </>
           ),
@@ -110,7 +112,7 @@ export default {
           contentComp: (
             <>
               <p>
-                You might think immediate improvements in air quality during shutdowns would be obvious. However, in reality, the way our atmosphere behaves isn’t so clear-cut. Nitrogen dioxide is only one component of air quality: sulfur dioxide, ozone, formaldehyde, and carbon dioxide along with a host of other atmospheric constituents also influence the quality of the air we breathe. The difference in nitrogen dioxide is that it has a relatively short lifetime in the atmosphere. In other words, once it’s emitted, it only lasts a few hours before it disappears. Therefore, once communities entered lockdowns and their mobility was severely restricted, the effect on NO<sub>2</sub> concentrations was the equivalent of flipping a switch. Be that as it may, this isn’t the case with all air pollutants.
+                Nitrogen dioxide is only one component of air quality: sulfur dioxide (SO<sub>2</sub>), ozone (O<sub>3</sub>), formaldehyde (CH<sub>2</sub>O), and carbon monoxide, along with a host of other atmospheric constituents, also influence the quality of the air we breathe. The difference in nitrogen dioxide is that it has a relatively short lifetime in the atmosphere; once it’s emitted, it only lasts a few hours before it disappears. Therefore, once communities entered shutdowns, and people’s mobility was severely restricted, the effect on nitrogen dioxide concentrations was the equivalent of flipping a switch. That is not, however, the case with all air pollutants.
               </p>
             </>
           ),
@@ -138,7 +140,7 @@ export default {
           contentComp: (
             <>
               <p>
-                Even with the strong correlation between NO<sub>2</sub> and the combustion of fossil fuels, the relationship between nitrogen dioxide and lockdowns isn’t straightforward either. Atmospheric concentrations of nitrogen dioxide naturally fluctuate throughout the year, and weather patterns also influence its concentrations. For example, nitrogen dioxide typically falls dramatically during spring and summer months, and rain and wind increase its dispersion, lowering local concentrations.Therefore, during the COVID-19 pandemic, scientists are careful to attribute observed changes solely to changes in our behavior and are also looking to determine whether some of the reductions are the result of natural variation.
+                Even with the strong correlation between nitrogen dioxide and the combustion of fossil fuels, atmospheric concentrations of nitrogen dioxide naturally fluctuate throughout the year, and weather patterns also influence its concentrations. For example, nitrogen dioxide typically falls dramatically during spring and summer months, and rain and wind increase its dispersion, lowering local concentrations. During the COVID-19 pandemic, NASA scientists have been able to attribute the observed changes in nitrogen dioxide to changes in our behavior, and they have been careful to account for any impacts on air pollution that are the result of natural weather variations.
               </p>
             </>
           ),
@@ -168,7 +170,7 @@ export default {
       contentComp: (
         <>
           <p>
-            NASA is able to observe global nitrogen dioxide levels using the OMI instrument (pronounced OH-me) aboard the Aura satellite. A joint endeavor between NASA and the Royal Netherlands Meteorological Institute (KNMI), OMI has been taking continuous global measurements of nitrogen dioxide since 2004. So, when the pandemic hit, OMI was ready to observe subsequent changes in NO<sub>2</sub> levels, and it continues to capture these measurements throughout various stages of the pandemic. NASA scientists are also leveraging other space-based instruments from international partners to study NO<sub>2</sub>. Specifically, the TROPOMI instrument aboard the European Commission’s Copernicus Sentinel-5P satellite helps us see nitrogen dioxide levels in greater detail. TROPOMI and OMI measurements make a great team: TROPOMI provides higher spatial resolution data than OMI, but OMI’s longer data record provides crucial context for TROPOMI observations.
+            NASA has used the Ozone Monitoring Instrument (OMI) aboard the Aura satellite to observe global nitrogen dioxide levels since 2004. A joint endeavor between NASA, the Royal Netherlands Meteorological Institute (KNMI) and the Finnish Meteorological Institute (FMI), OMI’s longer data record provides important context with which to compare any changes observed during the pandemic. NASA scientists are also leveraging other space-based instruments from international partners to study changes in nitrogen dioxide during the pandemic. These include the TROPOspheric Monitoring Instrument (TROPOMI) instrument aboard the European Commission’s Copernicus Sentinel-5P satellite. Launched in 2016, TROPOMI provides higher resolution observations than OMI.
           </p>
         </>
       ),
@@ -185,7 +187,7 @@ export default {
       contentComp: (
         <>
           <p>
-            Because nitrogen dioxide is primarily emitted from burning fossil fuels, changes in its atmospheric concentration can be tied to changes in human activity if the data are properly processed and interpreted. These connections are underscored when comparing different NASA datasets, like observations in changing nightlights during the pandemic. Here we see the illuminated web of highways connecting the Los Angeles metropolitan region. Researchers are using night light observations to track variations in energy use, migration, and transportation in response to social distancing and lockdown measures. These data, collected by the VIIRS instrument aboard the joint NASA-National Oceanic and Atmospheric Administration (NOAA) Suomi-NPP satellite, correlate with changes seen in car traffic on the ground – and, therefore, nitrogen dioxide reductions. While this research is still ongoing, the 31% reduction in NO<sub>2</sub> levels in Los Angeles during the height of pandemic-related lockdowns compared to recent years seems to correspond with a 15% reduction in nighttime lights over highways during the same period.
+            Changes in nighttime lights during the pandemic can also be tied to changes in nitrogen dioxide levels if the data are properly processed and interpreted.This is because nitrogen dioxide is primarily emitted from burning fossi fuels, and highways light up on nighttime satellite imagery when vehicles are present. Here we see the illuminated web of highways connecting the Los Angeles metropolitan region. Researchers are using night light observations to track variations in energy use, migration, and transportation in response to social distancing and shutdown measures during the pandemic. These data, collected by the Visible Infrared Imaging Radiometer Suite (VIIRS) instrument aboard the joint NASA-National Oceanic and Atmospheric Administration (NOAA) Suomi-National Polar-orbiting Partnership (NPP) satellite, correlate with changes seen in car traffic on the ground – and, therefore, nitrogen dioxide reductions. While this research is still ongoing, the 31% reduction in nitrogen dioxide levels in Los Angeles during the height of pandemic-related lockdowns compared to recent years seems to correspond with a 15% reduction in nighttime lights over highways during the same period.
           </p>
         </>
       ),
@@ -212,7 +214,7 @@ export default {
           contentComp: (
             <>
               <p>
-                New research during the pandemic is also looking at how COVID-related travel bans are impacting air quality around airports. Current conditions create a unique opportunity to study airport-related pollutants, especially nitrogen dioxide and formaldehyde. While travel bans and strict regulations around air travel are in place, air traffic has yet to return to normal levels, and many planes remain grounded. Here we see Planet Labs imagery using artificial intelligence to detect grounded airplanes at the Baltimore-Washington International Airport (BWI). During the pandemic, BWI reported that air traffic there had been reduced by around 60 percent. Scientists expect this footprint and the footprint of air travel across the world will likely return to its former shape as travel policies are gradually relaxed.
+                New research during the pandemic is also looking at how COVID-related travel bans are impacting air quality around airports. Current conditions create a unique opportunity to study airport-related pollutants, especially nitrogen dioxide and formaldehyde. While travel bans and strict regulations around air travel have been in place, air traffic has yet to return to previous levels, and many planes remain grounded. Here we see Planet Labs imagery using artificial intelligence to detect grounded airplanes at the Hartsfield-Jackson Atlanta International airport in March 2020. During this time, the airport reported a decline of 85% in passenger traffic.
               </p>
             </>
           )
@@ -224,7 +226,7 @@ export default {
           contentComp: (
             <>
               <p>
-                Dr. Jennifer Kaiser, the principal investigator behind the research studying air quality at airports during the pandemic, explained that during lockdowns, people are seeing better air quality with less traffic. Therefore, they may also wonder if this is what the future could look like if we relied more heavily on electric vehicles than we do now. Yet, airplanes are not going to be electric anytime soon, Kaiser explained. So, she and her team have installed two sensors at both BWI and the Hartsfield-Jackson Atlanta International Airport to identify air pollutant levels around airports during the pandemic. The sensors, which are part of NASA’s Pandora Project, use spectrometers to identify chemicals in the air. Kaiser and her team are comparing the on-the-ground sensor information with satellite information from TROPOMI. So far, she and her group have found that rebounds in nitrogen dioxide at airports have been slower to occur than those from automobile traffic.
+                Scientists behind the research studying air quality at airports during the pandemic explained that because people are seeing better air quality with less traffic during shutdowns, they may also wonder if this is what the future could look like if we relied more heavily on electric vehicles. Unlike cars, however, airplanes are not expected to become electric anytime soon. Therefore, Scientists installed two sensors at the Baltimore-Washington International Airport and two sensors at the Hartsfield-Jackson Atlanta International Airport to better characterize how airplanes contribute to air pollution. Researchers are comparing the on-the-ground sensor information with satellite information from TROPOMI. So far, they have found that nitrogen dioxide hotspots in Atlanta shifted from the airport, seen here, to the city center from April-June 2020. By September, however, satellites revealed the airport had reemerged as a dominant nitrogen dioxide emission source.
               </p>
             </>
           )
@@ -234,11 +236,11 @@ export default {
     },
     {
       id: 'seeing-rebounds',
-      name: 'Seeing Rebounds',
+      name: 'Seeing Rebounds in NO',
       contentComp: (
         <>
           <p>
-            After the initial shock of COVID-related shutdowns in the spring, communities worldwide have begun to reopen and gradually increase mobility. Cars have returned to the road, and travel restrictions have been slowly easing. These resumptions in behavior have subsequently corresponded with relative increases in nitrogen dioxide levels and other air pollutants, as air quality levels begin to return to pre-pandemic levels. Therefore, any long-term gains in overall air quality from the temporary reductions during short-term lockdowns appears to be minimal with regard to nitrogen dioxide. Looking to the future, NASA scientists will continue to monitor nitrogen dioxide levels and long-term trends around the world and at home. NASA is expected to launch its Tropospheric Emissions: Monitoring of Pollution, (TEMPO) instrument in 2022, which will provide hourly, high-resolution measurements of nitrogen dioxide, ozone, and other air pollutants across North America, revolutionizing future air quality forecasts.
+            After the initial shock of COVID-related shutdowns in the spring, communities worldwide began to reopen and gradually increase mobility. Cars returned to the road, and travel restrictions slowly eased. These resumptions corresponded with relative increases in nitrogen dioxide levels and other air pollutants, as air quality levels began to return to pre-pandemic levels. This demonstrates how quickly atmospheric nitrogen dioxide responds to reductions in emissions. They will persist as long as emissions persist and decline rapidly if emissions are reduced. NASA scientists will continue to monitor nitrogen dioxide levels and long-term trends around the world. NASA is expected to launch its Tropospheric Emissions: Monitoring of Pollution, (TEMPO) instrument in 2022, which will provide hourly, high-resolution measurements of nitrogen dioxide, ozone, and other air pollutants across North America, improving future air quality forecasts.
           </p>
         </>
       ),
