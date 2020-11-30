e62c4d38d1c4f8ad1914624527424b9de810f91d
Olaf Veerman <olaf.veerman@gmail.com>
2020-11-30 21:54:30 +0000

    Replace Lorem ipsum of the Stories Hub

--- app/assets/scripts/components/stories/hub/index.js
+++ app/assets/scripts/components/stories/hub/index.js
@@ -68,19 +68,7 @@ const StoriesHub = (props) => {
             <HubFold>
               <Prose>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum fringilla vulputate porttitor. Donec feugiat
-                  aliquam libero, id finibus risus mattis quis. Morbi ornare sit
-                  amet mi ut efficitur. Proin sodales tellus enim, quis feugiat
-                  purus facilisis quis. Praesent facilisis, quam et eleifend
-                  finibus, erat urna porttitor sem, at suscipit urna libero eu
-                  felis. Duis ullamcorper lectus ut tempus interdum. Donec
-                  lobortis, ante sit amet euismod sollicitudin, nisi lectus
-                  finibus enim, id mollis eros sapien in neque. Aenean fermentum
-                  mauris sed nunc posuere venenatis. Vestibulum mi ex, sagittis
-                  a semper non, pretium a purus. Orci varius natoque penatibus
-                  et magnis dis parturient montes, nascetur ridiculus mus. Donec
-                  id libero lobortis, scelerisque est vitae, fringilla sapien.
+                  Explore the guided narratives below to discover how NASA satellites and other Earth observing resources reveal how the COVID-19 pandemic is affecting Earth’s air, land, water, and climate.
                 </p>
               </Prose>
             </HubFold>
